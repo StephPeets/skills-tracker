5b1b42ff875c6eaafbb1f088833a20c89d567039
Steph <84749465+StephPeets@users.noreply.github.com>
2021-10-18 20:29:28 -0400

    adjust spacing

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -10,92 +10,91 @@ public class SkillsController {
 
 	@GetMapping
 	public String defaultRequest() {
-		return
-			"<html>" +
-				"<body>" +
-				"<h1>Skills Tracker</h1>" +
-				"<h2>" +
-				"We have several skills we would like to learn. Here's the list!" +
-				"</h2>" +
-				"<ul>" +
-				"<li>JavaScript</li>" +
-				"<li>Java</li>" +
-				"<li>Python</li>" +
-				"<li>Kotlin</li>" +
-				"<li>Go</li>" +
-				"</ul>" +
-				"<br>" +
+		return	"<html>" +
+			"<body>" +
+			"<h1>Skills Tracker</h1>" +
+			"<h2>" +
+			"We have several skills we would like to learn. Here's the list!" +
+			"</h2>" +
+			"<ul>" +
+			"<li>JavaScript</li>" +
+			"<li>Java</li>" +
+			"<li>Python</li>" +
+			"<li>Kotlin</li>" +
+			"<li>Go</li>" +
+			"</ul>" +
+			"<br>" +
 
-				"<h2>" +
-				"What are your top 3 favorites?" +
-				"</h2>" +
+			"<h2>" +
+			"What are your top 3 favorites?" +
+			"</h2>" +
 
-				"<form action='choices' method='post'>" +
-				"Name:" +
-				"<br>" +
-				"<input type='text' name='name' placeholder='Your Name' required>" +
-				"<br>" +
+			"<form action='choices' method='post'>" +
+			"Name:" +
+			"<br>" +
+			"<input type='text' name='name' placeholder='Your Name' required>" +
+			"<br>" +
 
-				"First:" +
-				"<br>" +
-				"<select name='first'>" +
-				"<option value='JavaScript'>JavaScript</option>" +
-				"<option value='Java'>Java</option>" +
-				"<option value='Python'>Python</option>" +
-				"<option value='Kotlin'>Kotlin</option>" +
-				"<option value='Go'>Go</option>" +
-				"</select>" +
-				"<br>" +
+			"First:" +
+			"<br>" +
+			"<select name='first'>" +
+			"<option value='JavaScript'>JavaScript</option>" +
+			"<option value='Java'>Java</option>" +
+			"<option value='Python'>Python</option>" +
+			"<option value='Kotlin'>Kotlin</option>" +
+			"<option value='Go'>Go</option>" +
+			"</select>" +
+			"<br>" +
 
-				"Second:" +
-				"<br>" +
-				"<select name='second'>" +
-				"<option value='JavaScript'>JavaScript</option>" +
-				"<option value='Java'>Java</option>" +
-				"<option value='Python'>Python</option>" +
-				"<option value='Kotlin'>Kotlin</option>" +
-				"<option value='Go'>Go</option>" +
-				"</select>" +
-				"<br>" +
+			"Second:" +
+			"<br>" +
+			"<select name='second'>" +
+			"<option value='JavaScript'>JavaScript</option>" +
+			"<option value='Java'>Java</option>" +
+			"<option value='Python'>Python</option>" +
+			"<option value='Kotlin'>Kotlin</option>" +
+			"<option value='Go'>Go</option>" +
+			"</select>" +
+			"<br>" +
 
-				"Third:" +
-				"<br>" +
-				"<select name='third'>" +
-				"<option value='JavaScript'>JavaScript</option>" +
-				"<option value='Java'>Java</option>" +
-				"<option value='Python'>Python</option>" +
-				"<option value='Kotlin'>Kotlin</option>" +
-				"<option value='Go'>Go</option>" +
-				"</select>" +
-				"<br>" +
+			"Third:" +
+			"<br>" +
+			"<select name='third'>" +
+			"<option value='JavaScript'>JavaScript</option>" +
+			"<option value='Java'>Java</option>" +
+			"<option value='Python'>Python</option>" +
+			"<option value='Kotlin'>Kotlin</option>" +
+			"<option value='Go'>Go</option>" +
+			"</select>" +
+			"<br>" +
 
-				"<input type='submit' value ='Submit'>" +
-				"</form>" +
-				"</body>" +
-				"</html>";
+			"<input type='submit' value ='Submit'>" +
+			"</form>" +
+			"</body>" +
+			"</html>";
 	}
 
 	@PostMapping("choices")
 	public String choices(@RequestParam String name, String first, String second, String third) {
 		return "<html>" +
-			       "<body>" +
-			       "<h1>" +
-			       name + "'s Top 3 Coding Languages" +
-			       "</h1>" +
-			       "<h2>" +
-			       "<ol>" +
-			       "<li>" +
-			       first +
-			       "</li>" +
-			       "<li>" +
-			       second +
-			       "</li>" +
-			       "<li>" +
-			       third +
-			       "</li>" +
-			       "</ol>" +
-			       "</h2>" +
-			       "</body>" +
-			       "</html>";
+		       "<body>" +
+		       "<h1>" +
+		       name + "'s Top 3 Coding Languages" +
+		       "</h1>" +
+		       "<h2>" +
+		       "<ol>" +
+		       "<li>" +
+		       first +
+		       "</li>" +
+		       "<li>" +
+		       second +
+		       "</li>" +
+		       "<li>" +
+		       third +
+		       "</li>" +
+		       "</ol>" +
+		       "</h2>" +
+		       "</body>" +
+		       "</html>";
 	}
 }
